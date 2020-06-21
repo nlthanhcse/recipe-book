233f96562599c91ddd7f5a291db2808b192886d2
nlthanhcse <nlthanhititiu17038@gmail.com>
2020-06-21 22:30:23 +0700

    add ingredient list property to recipe model

--- src/app/recipe/recipe.model.ts
+++ src/app/recipe/recipe.model.ts
@@ -1,4 +1,9 @@
+import {Ingredient} from '../shared/ingredient.model';
+
 export class Recipe {
-  constructor(public name: string, public description: string, public imgPath: string) {
+  constructor(public name: string,
+              public description: string,
+              public imgPath: string,
+              public ingredients: Ingredient[]) {
   }
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,12 +1,25 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   private recipeClicked = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Recipe 1 name', 'Recipe 1 description', 'https://exp.gg/vn/wp-content/uploads/2020/04/chillout-1200x900.jpg'),
-    new Recipe('Recipe 2 name', 'Recipe 2 description', 'https://i.ytimg.com/vi/WPXk8uaOcA0/maxresdefault.jpg')
+    new Recipe('Recipe 1 name',
+      'Recipe 1 description',
+      'https://exp.gg/vn/wp-content/uploads/2020/04/chillout-1200x900.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Carrot', 20)
+      ]),
+    new Recipe('Recipe 2 name',
+      'Recipe 2 description',
+      'https://i.ytimg.com/vi/WPXk8uaOcA0/maxresdefault.jpg',
+      [
+        new Ingredient('Beef', 2),
+        new Ingredient('Onion', 10)
+      ])
   ];
 
   public getAll() {
